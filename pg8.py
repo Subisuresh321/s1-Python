5054c6468dd73a544173ed83ee4fc14b27cba9c3
Subi Suresh <162290234+Subisuresh321@users.noreply.github.com>
2025-01-02 23:07:30 +0530

    Update pg8.py

--- pg8.py
+++ pg8.py
@@ -1,16 +1,5 @@
-num1=input("Enter a number : ")
-temp=num1
-digits=0
-c=False
-
-if float(num1)==0:
-    print(f"There are 0 digits in {num1}")
-else:
-    for i in num1:
-        if i==".":
-            c=True
-        digits+=1
-
-    if c is True:
-        digits-=1
-    print(f"There are {digits} digits in {temp}")   
+num1=int(input("Enter a number : "))
+count=0
+for i in str(num1):
+    count+=1
+print(count)
